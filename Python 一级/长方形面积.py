e5f716420cd225dcca171ceff83f3b7eddf381f2
Caldson <minecraftcaldson@outlook.com>
2026-02-25 12:18:43 +0800

    发布一个

--- Python 一级/长方形面积.py
+++ Python 一级/长方形面积.py
@@ -1,1 +1,7 @@
-print(int(int(input()) ** 0.5))
+a = int(input())
+result = 0
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        if i*j==a and (i >= j):
+            result += 1
+print(result)
